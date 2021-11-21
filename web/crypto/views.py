e14184645be9708b28cdb1cc873453393142aad9
ShallowSmile1 <Gritsenko_egor@mail.ru>
2021-11-22 01:40:54 +0300

    Redirection to signup created

--- web/crypto/views.py
+++ web/crypto/views.py
@@ -12,18 +12,28 @@ from kyc import AccountManager
 
 account = AccountManager()
 
-#def get_redirect_url
-
 def home_page(request):
-    context = {
-        'account': account
-    }
-    return render(request,'home.html', context)
+    if account.is_logged_in():
+        balance = account.get_balance()
+        context = {
+            'account': account,
+            'balance': balance
+        }
+        return render(request,'home.html', context)
+    else:
+        return signup_page(request)
     
 def transaction_page(request):
- #   if account.is_logged_in:
-#      context
-        return render(request, 'transactions.html')
+    if account.is_logged_in():
+        payments = account.show_payments()
+        context = {
+            'payments': payments,
+            'account': account
+        }
+        return render(request, 'transactions.html', context)
+    else:
+        return signup_page(request)
+    
     
 def signup_page(request):
     if request.method == 'POST':
@@ -45,13 +55,21 @@ def signup_page(request):
     
 
 def send_token_page(request):
-    if request.method == 'POST':
-        form = TokenForm(request.POST)
-        if form.is_valid():
-            address = form.cleaned_data['address']
-            value = form.cleaned_data['value']
-            return render(request, 'transactions.html')
-    else:
-        form = TokenForm()
-    
-    return render(request, 'new_token.html', {'form': form})   
+    if account.is_logged_in():
+        context = {
+                    'account': account
+        }
+        if request.method == 'POST':
+            form = TokenForm(request.POST)
+            if form.is_valid():
+                address = form.cleaned_data['address']
+                value = form.cleaned_data['value']
+                account.make_transaction(address, value)
+                payments = account.show_payments()
+                return render(request, 'transactions.html', context)
+            else:
+                return signup_page(request)
+        else:
+            form = TokenForm()
+        return render(request, 'new_token.html', {'form': form, 'account': account})
+    return signup_page(request)
