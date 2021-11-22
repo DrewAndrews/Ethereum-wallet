46d6367f3a4f2314e85e7e17eedd3967244d2a2e
ShallowSmile1 <Gritsenko_egor@mail.ru>
2021-11-22 08:55:38 +0300

    bootstrap, crispy and css corrections

--- web/crypto/forms.py
+++ web/crypto/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.forms.widgets import NumberInput
 
 
 class TokenForm(forms.Form):
--- web/crypto/views.py
+++ web/crypto/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.views.generic.edit import FormView
 from .forms import TokenForm, UserForm
-from django.urls import reverse_lazy
+
 import sys
 
 sys.path.append('../blockchain/')
@@ -15,10 +13,12 @@ account = AccountManager()
 def home_page(request):
     if account.is_logged_in():
         balance = account.get_balance()
+        
         context = {
             'account': account,
             'balance': balance
         }
+        
         return render(request,'home.html', context)
     else:
         return signup_page(request)
@@ -26,10 +26,12 @@ def home_page(request):
 def transaction_page(request):
     if account.is_logged_in():
         payments = account.show_payments()
+        
         context = {
             'payments': payments,
             'account': account
         }
+        
         return render(request, 'transactions.html', context)
     else:
         return signup_page(request)
@@ -38,15 +40,17 @@ def transaction_page(request):
 def signup_page(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
-        
-        context = {
-            'account': account
-        }
-        
         if form.is_valid():
             phone = form.cleaned_data['phone']
             password = form.cleaned_data['password']
             account.login(phone, password)
+            balance = account.get_balance()
+            
+            context = {
+                'account': account,
+                'balance': balance
+            }
+            
             return render(request,'home.html', context)
     else:
         form = UserForm()
@@ -56,16 +60,17 @@ def signup_page(request):
 
 def send_token_page(request):
     if account.is_logged_in():
+        
         context = {
                     'account': account
         }
+        
         if request.method == 'POST':
             form = TokenForm(request.POST)
             if form.is_valid():
                 address = form.cleaned_data['address']
                 value = form.cleaned_data['value']
                 account.make_transaction(address, value)
-                payments = account.show_payments()
                 return render(request, 'transactions.html', context)
             else:
                 return signup_page(request)
