b5d2f80b86bb3719c86353c5b4931e614aaa959f
a.rusinovich <a.rusinovich@avsw.ru>
2021-11-25 21:46:44 +0300

    :)

--- web/web/settings.py
+++ web/web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yvq*_6-$*mem3f$kpljmq$kh%%^$iu1lexxos91xc^*6gy^b8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
